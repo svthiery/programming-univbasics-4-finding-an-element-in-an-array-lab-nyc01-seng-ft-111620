24ec9d3489d526bb72a82770f625679d7bed6605
Sarah Kain Thiery <sarahvkain@gmail.com>
2020-10-27 17:35:16 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,10 +1,12 @@
 def find_element_index(array, value_to_find)
   count = 0 
+  
   while count < array.length do
     if array[count] == value_to_find
       index_to_find = count
       index_to_find
     end
+    
     count += 1 
   end
 end
